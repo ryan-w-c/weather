86c4361774ca058cc7500815127fc3d7f009de81
ryan-w-c <cavanaghryan1@gmail.com>
2024-08-09 23:24:55 -0700

    update function js file to be correct format for netlify

--- functions/api-proxy.js
+++ functions/api-proxy.js
@@ -1,18 +1,4 @@
-exports.handler = async (event, context) => {
-    try {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
-
-        const data = await response.json();
-        return {
-            statusCode: 200,
-            body: JSON.stringify(data),
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                error: 'Internal Server Error' 
-            }),
-        };
-    }
+export default async (req, context) => {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+    return new Response(response);
 };
