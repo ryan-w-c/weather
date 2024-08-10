35f66312edb54cc5e36e0b1bb8adf291b0b43e0f
ryan-w-c <cavanaghryan1@gmail.com>
2024-08-09 23:32:37 -0700

    update function js file to be correct format for netlify

--- functions/api-proxy.js
+++ functions/api-proxy.js
@@ -1,4 +1,7 @@
 export default async (req, context) => {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
-    return new Response(response);
-};
+    const data = await response.json();
+    return new Response(JSON.stringify(data), { 
+      headers: { 'Content-Type': 'application/json' },
+    });
+  };
