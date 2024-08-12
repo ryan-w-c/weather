b0199330431a7b202569cc6255739b1acd860f63
ryan-w-c <cavanaghryan1@gmail.com>
2024-08-11 21:54:12 -0700

    upload search js for algolia search through netlify

---
+++ functions/search.js
@@ -0,0 +1,17 @@
+const algoliasearch = require('algoliasearch');
+const instantsearch = require('instantsearch.js');
+
+export default async (req, context) => {
+  const searchClient = algoliasearch(process.env.ALGOLIA_APP_ID, ALGOLIA_API_KEY);
+  const search = instantsearch('worldcities', searchClient);
+
+  const { query } = JSON.parse(req.body);
+  const { hits } = await search.search(query, {
+    attributesToRetrieve: ['city', 'iso2', '_geoloc.lat', '_geoloc.lng']
+  });
+
+  const data = await hits.json();
+  return new Response(JSON.stringify(data), { 
+    headers: { 'Content-Type': 'application/json' },
+  });
+};
