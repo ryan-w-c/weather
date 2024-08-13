302a7f22a0c131c50f5b34a460c078d41d263204
ryan-w-c <cavanaghryan1@gmail.com>
2024-08-12 21:01:44 -0700

    update search.js

--- functions/search.js
+++ functions/search.js
@@ -3,15 +3,15 @@ const instantsearch = require('instantsearch.js');
 
 export default async (req, context) => {
   const searchClient = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY);
-  const search = instantsearch('worldcities', searchClient);
+  const search = searchClient.initIndex('worldcities');
 
-  const { query } = JSON.parse(req.body);
+  const url = new URL(req.url);
+  const query = url.searchParams.get('query') || '';
   const { hits } = await search.search(query, {
-    attributesToRetrieve: ['city', 'iso2', '_geoloc.lat', '_geoloc.lng']
+    attributesToRetrieve: ['city', 'iso2', 'lat', 'lng']
   });
 
-  const data = await hits.json();
-  return new Response(JSON.stringify(data), { 
+  return new Response(JSON.stringify(hits), { 
     headers: { 'Content-Type': 'application/json' },
   });
 };
